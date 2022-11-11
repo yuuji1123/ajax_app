336172381970a5015a1d1ada7acd7321ae7b262f
yuuji1123 <wagnas.syoukankyou@gmail.com>
2022-11-11 15:55:12 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "DESC")
   end
 
-  def new
-  end
+    # コメントアウトする
+    # def new
+    # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index  # 追記する
   end
 end
